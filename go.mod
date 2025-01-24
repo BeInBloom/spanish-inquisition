b4dcce3a00a4630cfc869c1a7ebd958228848e5b
Sergei Skoropupov <donckou@gmail.com>
2025-01-25 02:47:27 +0300

    some fixing for pass test for 2 iter kekw

--- go.mod
+++ go.mod
@@ -1,18 +1,15 @@
 module github.com/BeInBloom/spanish-inquisition
 
-go 1.22
-
-require (
-	github.com/go-chi/chi/v5 v5.2.0
-	github.com/spf13/pflag v1.0.5
-	gopkg.in/yaml.v3 v3.0.1
-)
+go 1.22.11
 
 require (
 	github.com/caarlos0/env/v11 v11.3.1
 	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.2.0
+	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.10.0
 	go.uber.org/zap v1.27.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
--- internal/data-saver/httpsaver/httpsaver_test.go
+++ internal/data-saver/httpsaver/httpsaver_test.go
@@ -11,29 +11,6 @@ import (
 )
 
 func TestHttpSaver_Save_Success(t *testing.T) {
-	// mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// 	assert.Equal(t, "POST", r.Method)
-	// 	assert.Equal(t, "text/plain", r.Header.Get("Content-Type"))
-	// 	w.WriteHeader(http.StatusOK)
-	// }))
-	// defer mockServer.Close()
-
-	// // Конфигурация saver
-	// saverConfig := config.SaverConfig{
-	// 	Timeout: 2,
-	// 	URL:     mockServer.URL + "/%s/%s/%s",
-	// }
-
-	// saver := New(saverConfig)
-
-	// data := ptypes.SendData{
-	// 	MetricType: "gauge",
-	// 	Name:       "test-metric",
-	// 	Value:      "42",
-	// }
-
-	// err := saver.Save(data)
-	// assert.NoError(t, err)
 }
 
 func TestHttpSaver_Save_HttpError(t *testing.T) {
@@ -60,25 +37,4 @@ func TestHttpSaver_Save_HttpError(t *testing.T) {
 }
 
 func TestHttpSaver_Save_Timeout(t *testing.T) {
-	// mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// 	time.Sleep(3 * time.Second)
-	// }))
-	// defer mockServer.Close()
-
-	// saverConfig := config.SaverConfig{
-	// 	Timeout: 1,
-	// 	URL:     mockServer.URL + "/%s/%s/%s",
-	// }
-
-	// saver := New(saverConfig)
-
-	// data := ptypes.SendData{
-	// 	MetricType: "gauge",
-	// 	Name:       "test-metric",
-	// 	Value:      "42",
-	// }
-
-	// err := saver.Save(data)
-	// assert.Error(t, err)
-	// assert.Contains(t, err.Error(), "Client.Timeout exceeded")
 }
