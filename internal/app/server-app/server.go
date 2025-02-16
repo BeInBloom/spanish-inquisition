1169a8f07dba78b47368ed88f3639abf805b1336
Sergei Skoropupov <donckou@gmail.com>
2025-02-16 05:43:24 +0300

    some fix

--- internal/app/server-app/server.go
+++ internal/app/server-app/server.go
@@ -104,11 +104,11 @@ func (a *app) initHandlers() {
 		r.Route("/value", func(r chi.Router) {
 			r.With(middleware.AllowContentType("application/json")).Get("/", handlers.GetDataByJSON(a.repo))
 			r.With(middleware.AllowContentType("application/json")).Post("/", handlers.GetDataByJSON(a.repo))
-			r.With(middleware.AllowContentType("text/plain")).Get("/{type}/{name}/", handlers.GetData(a.repo))
+			r.With(middleware.AllowContentType("text/plain")).Get("/{type}/{name}", handlers.GetData(a.repo))
 		})
 		r.Route("/update", func(r chi.Router) {
 			r.With(middleware.AllowContentType("application/json")).Post("/", handlers.CreateOrUpdateByJSON(a.repo))
-			r.With(middleware.AllowContentType("text/plain")).Post("/{type}/{name}/{value}/", handlers.CreateOrUpdate(a.repo))
+			r.With(middleware.AllowContentType("text/plain")).Post("/{type}/{name}/{value}", handlers.CreateOrUpdate(a.repo))
 		})
 	})
 
